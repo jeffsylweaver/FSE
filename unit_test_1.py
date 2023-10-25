8b0ef19969ca520ac9bf2c7adf76b30f71026334
Jeff <weaverjs@vcu.edu>
2023-10-24 22:44:09 -0400

    2nd commit| 1st test

---
+++ unit_test_1.py
@@ -0,0 +1,32 @@
+import program
+import unittest
+
+    class TestSum(unittest.TestCase):
+        def test_integer(self):
+            self.assertEqual(program.subtract(2,2),0)
+
+
+
+    if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
